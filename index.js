2fca83cbd36760f9bd8c2e2c0ba34211ed0d68c7
Olzhas Alexandrov <olzhas@alexandrov.co>
2018-05-18 08:04:25 -0500

    return raw svg

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-const svgo = require('svgo');
-const loaderUtils = require('loader-utils');
-const compiler = require('vue-template-compiler');
-const transpile = require('vue-template-es2015-compiler');
+const svgo = require("svgo")
+const loaderUtils = require("loader-utils")
+const compiler = require("vue-template-compiler")
+const transpile = require("vue-template-es2015-compiler")
 
-module.exports = function (content) {
-  const options = loaderUtils.getOptions(this) || {};
-  const svg = new svgo(options.svgo || {});
-  const path = this.resourcePath;
+module.exports = function(content) {
+  const options = loaderUtils.getOptions(this) || {}
+  const svg = new svgo(options.svgo || {})
+  const path = this.resourcePath
 
-  this.cacheable && this.cacheable(true);
-  this.addDependency(this.resourcePath);
+  this.cacheable && this.cacheable(true)
+  this.addDependency(this.resourcePath)
 
-  const cb = this.async();
-  let component;
+  const cb = this.async()
 
   svg
     .optimize(content, { path: path })
-    .then((result) => {
-      const compiled = compiler.compile(result.data, { preserveWhitespace: false });
-
-      component = transpile(`var render = function () {${compiled.render}};`);
-      component += `var toString = function () {return ${JSON.stringify(path)}};`;
-      component += `module.exports = { render: render, toString: toString };`;
-
-      cb(null, component);
+    .then(result => {
+      cb(null, result.data)
     })
-    .catch(cb);
-};
+    .catch(cb)
+}
